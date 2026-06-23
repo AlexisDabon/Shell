511a83ff64b036a3bba97b4fb6ce5f23a48fd643
AlexisDabon <xela619375@gmail.com>
2026-06-23 15:49:03 +0200

    exercise completed

--- guess_code.sh
+++ guess_code.sh
@@ -3,7 +3,21 @@
 CODE_CORRECT=31
 ESSAI=0
 
-while [ $ESSAI -ne $CODE_CORRECT ]; do
-	echo "devinez le code secret :";
-	read ESSAI;
+while [ "$ESSAI" -ne "$CODE_CORRECT" ]; do
+	echo "devinez le code secret :"
+	read ESSAI
+
+if [ "$ESSAI" -lt "$CODE_CORRECT" ]; then
+	echo "C'est plus grand"
+
+elif [ "$ESSAI" -gt "$CODE_CORRECT" ]; then
+	echo "C'est plus petit"
+
+else 
+	echo "Bravo !"
+
+fi
+
 done
+
+
