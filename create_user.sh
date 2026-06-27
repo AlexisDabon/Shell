1f804f63fbab174fe32be693995bc233cfcb4428
AlexisDabon <xela619375@gmail.com>
2026-06-27 08:13:02 +0200

    exercise completed

--- create_user.sh
+++ create_user.sh
@@ -4,3 +4,13 @@ if [ $# -ne 2 ]; then
 	echo "usage $0 <nom_utilisateur> <age>"
 	exit 1
 fi
+
+USERNAME=$1
+AGE=$2
+
+if [ "$AGE" -le 0 ] 2>/dev/null; then
+	echo "Le nombre doit être un entier positif"
+	exit 1
+fi
+
+echo "Succès : Utilisateur $USERNAME ($AGE ans) créé avec succès !"
