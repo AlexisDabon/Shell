431c7ef199bc1170515718a34ed03e64056e7739
AlexisDabon <xela619375@gmail.com>
2026-06-29 11:53:09 +0200

    exercise completed

--- config_action.sh
+++ config_action.sh
@@ -5,3 +5,9 @@ REPONSE=""
 while [[ "$REPONSE" != "o" && "$REPONSE" != "n" ]]; do
 	read -p "voulez vous installer la mise à jour ? o/n" REPONSE
 done
+
+if [[ "$REPONSE" == o ]]; then
+	echo "installation de la mise à jour"
+else
+	echo "installation refusé"
+fi
