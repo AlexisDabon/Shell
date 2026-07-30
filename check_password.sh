99a998344469216a82b9a6b1d833a8b99a59c7eb
AlexisDabon <xela619375@gmail.com>
2026-07-30 07:48:15 +0200

    secure password validation script

--- check_password.sh
+++ check_password.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "entrez le mot de passe :"
-read SECRET
-if [ "$SECRET" = "1234" ]; then
-	echo "accès autorisé ."
-else 
-	echo "accès refusé ."
-fi
+set -euo pipefail
 
+read -sp "Entrez le mot de passe : " secret
+	echo ""
+
+if [ "$secret" = "1234" ]; then
+	echo "Accès autoisé"
+else
+	echo "Accès refusé"
+	exit 1
+fi
