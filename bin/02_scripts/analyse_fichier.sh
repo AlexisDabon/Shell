eec1085fba3b8e0b03e757b4e833d43874188b9a
AlexisDabon <xela619375@gmail.com>
2026-08-05 05:59:14 +0200

    feat: add lines counting and script exit

--- bin/02_scripts/analyse_fichier.sh
+++ bin/02_scripts/analyse_fichier.sh
@@ -11,10 +11,19 @@ readonly LIGNES_MAX="$2"
 
 if [[ ! "$LIGNES_MAX" =~ ^[1-9][0-9]*$ ]]; then
 	echo "Erreur: Le nombre maximum de lignes doit être un entier positif." >&2
-	exit 1 
+	exit 1
 fi
 
 if [[ ! -f "$CHEMIN_FICHIER" ]]; then
 	echo "Erreur: Le chemin '$CHEMIN_FICHIER' n'existe pas." >&2
 	exit 2
+fi
+
+readonly NB_LIGNES=$(wc -l < "$CHEMIN_FICHIER")
+
+if (( $NB_LIGNES > $LIGNES_MAX )); then
+	echo "Avertissement: Le fichier contient $NB_LIGNES lignes, ce qui dépasse la limite autorisée de $LIGNES_MAX" >&2
+	exit 3
+else
+	echo "Fichier conforme ($NB_LIGNES / $LIGNES_MAX lignes)"
 fi
