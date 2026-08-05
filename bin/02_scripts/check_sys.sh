ce4a27e6f484ec0f2658d2097ae006fafee763e3
AlexisDabon <xela619375@gmail.com>
2026-08-05 20:00:13 +0200

    feat: check RAM with free and awk

--- bin/02_scripts/check_sys.sh
+++ bin/02_scripts/check_sys.sh
@@ -4,3 +4,8 @@ set -euo pipefail
 HOST_NAME=$(hostname)
 SYS_KERNEL=$(uname -sr)
 CLEAN_UPTIME=$(uptime -p | sed 's/^up //')
+
+echo "=== INFOS SYS. $HOST_NAME ($SYS_KERNEL) | Uptime: $CLEAN_UPTIME ==="
+
+echo "[RAM]"
+free -h | awk '/Mem:/ {printf "Total: %s | Utilisé: %s | Disponible: %s \n", $2, $3, $7}'
