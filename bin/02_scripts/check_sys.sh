0a4ec390fa37054f827ff18ec097fe0212e96208
AlexisDabon <xela619375@gmail.com>
2026-08-06 04:35:48 +0200

    feat: add stockage diagnostic

--- bin/02_scripts/check_sys.sh
+++ bin/02_scripts/check_sys.sh
@@ -9,3 +9,6 @@ echo "=== INFOS SYS. $HOST_NAME ($SYS_KERNEL) | Uptime: $CLEAN_UPTIME ==="
 
 echo "[RAM]"
 free -h | awk '/Mem:/ {printf "Total: %s | Utilisé: %s | Disponible: %s \n", $2, $3, $7}'
+
+echo "[STOCKAGE]"
+df -h --total | grep -E '^/dev/|^total' | grep -v 'loop'
