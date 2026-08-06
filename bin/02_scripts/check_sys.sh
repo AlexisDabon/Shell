82b25142ad07de7ef53902a0bb18077470a73be3
AlexisDabon <xela619375@gmail.com>
2026-08-06 07:24:42 +0200

    feat: last CPU/print step

--- bin/02_scripts/check_sys.sh
+++ bin/02_scripts/check_sys.sh
@@ -12,3 +12,10 @@ free -h | awk '/Mem:/ {printf "Total: %s | Utilisé: %s | Disponible: %s \n", $2
 
 echo "[STOCKAGE]"
 df -h --total | grep -E '^/dev/|^total' | grep -v 'loop'
+
+echo "[CPU]"
+if [ -r /proc/loadavg ]; then
+	awk '{printf "1 min: %s | 5 min: %s | 15 min: %s \n", $1, $2, $3}' /proc/loadavg
+else
+	LC_ALL=C uptime | awk -F'[lL]oad average[s]*:' '{printf "1, 5, 15 min: %s \n", $2}'
+fi
