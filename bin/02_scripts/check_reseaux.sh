c03a884550e172ccc63d314d33bbc1aa67edc146
AlexisDabon <xela619375@gmail.com>
2026-08-12 06:40:40 +0200

    feat: initialize ANSI Colors & counters

--- bin/02_scripts/check_reseaux.sh
+++ bin/02_scripts/check_reseaux.sh
@@ -13,3 +13,13 @@ if ! [[ -f "$CONF_FILE" && -r "$CONF_FILE" ]]; then
 	echo "Erreur le fichier '$CONF_FILE' n'existe pas ou est inaccessible." >&2
 	exit 1
 fi
+
+readonly COLOR_RED=$'\e[31m'
+readonly COLOR_GREEN=$'\e[32m'
+readonly COLOR_RESET=$'\e[0m'
+
+total_services=0
+services_online=0
+services_offline=0
+
+
